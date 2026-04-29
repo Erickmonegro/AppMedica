7e6467fedfdf506c9813db722677e58b76ae4dc5
Erick Junior Monegro Rodriguez <20251186@itla.edu.do>
2026-04-29 11:09:11 -0400

    base

---
+++ src/test/java/com/proyectomedico/appmedicacenfasies/AppMedicaCenfasiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectomedico.appmedicacenfasies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMedicaCenfasiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
